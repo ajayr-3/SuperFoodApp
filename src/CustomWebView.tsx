3bd86ca022c77d5070636d06f1cfe65cd788c7df
Ajay <ajay.r@primathon.in>
2024-05-09 13:47:31 +0530

    able to fetch page urls of webview

--- src/CustomWebView.tsx
+++ src/CustomWebView.tsx
@@ -1,4 +1,10 @@
-import React, {Dispatch, SetStateAction, useCallback, useRef} from 'react';
+import React, {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useRef,
+  useState,
+} from 'react';
 import {Text, TouchableOpacity, View, ViewStyle} from 'react-native';
 import WebView, {WebViewNavigation} from 'react-native-webview';
 import useStyles from './styles';
@@ -14,6 +20,7 @@ const CustomWebView = ({
 }) => {
   const styles = useStyles();
   const webViewRef = useRef<WebView | null>();
+  const [currentURI, setCurrentURI] = useState(url);
 
   const handleWebViewNavigationStateChange = useCallback(
     async (newNavState: WebViewNavigation) => {
@@ -25,13 +32,26 @@ const CustomWebView = ({
     },
     [],
   );
+
   return (
     <View style={styles.flex1}>
       <WebView
         ref={ref => (webViewRef.current = ref)}
         source={{uri: url}}
         style={style}
+        allowsBackForwardNavigationGestures
         onNavigationStateChange={handleWebViewNavigationStateChange}
+        onShouldStartLoadWithRequest={request => {
+          // not working
+          console.log(request);
+          // If we're loading the current URI, allow it to load
+          if (request.url === currentURI) {
+            return true;
+          }
+          // We're loading a new URL -- change state first
+          setCurrentURI(request.url);
+          return true;
+        }}
       />
       <View style={styles.crossBtnContainer}>
         <TouchableOpacity
